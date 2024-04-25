3285e96d9a002e55b2fad8b4a30ff631f46cea67
Evan Bartholomew <evanbartholomew@Evans-MacBook-Air.local>
2024-04-24 23:21:55 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,111 @@
+import java.util.Scanner;
+
+public class Main {
+    private static final int ROW = 3;
+    private static final int COL = 3;
+    private static String[][] board = new String[ROW][COL];
+    private static String currentPlayer = "X";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        clearBoard();
+
+        while (true) {
+            display();
+            int row, col;
+
+            do {
+                System.out.print("Player " + currentPlayer + ", enter your move (row[1-3] col[1-3]): ");
+                row = SafeInput.getRangedInt(scanner, "", 1, 3) - 1;
+                col = SafeInput.getRangedInt(scanner, "", 1, 3) - 1;
+            } while (!isValidMove(row, col));
+
+            board[row][col] = currentPlayer;
+
+            if (isWin(currentPlayer)) {
+                display();
+                System.out.println("Player " + currentPlayer + " wins!");
+                break;
+            } else if (isTie()) {
+                display();
+                System.out.println("It's a tie!");
+                break;
+            }
+
+            currentPlayer = currentPlayer.equals("X") ? "O" : "X";
+        }
+
+        scanner.close();
+    }
+
+    private static void clearBoard() {
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COL; j++) {
+                board[i][j] = " ";
+            }
+        }
+    }
+
+    private static void display() {
+        System.out.println("  1 2 3");
+        for (int i = 0; i < ROW; i++) {
+            System.out.print(i + 1 + " ");
+            for (int j = 0; j < COL; j++) {
+                System.out.print(board[i][j]);
+                if (j < COL - 1) {
+                    System.out.print("|");
+                }
+            }
+            System.out.println();
+            if (i < ROW - 1) {
+                System.out.println("  -----");
+            }
+        }
+    }
+    private static boolean isValidMove(int row, int col) {
+        if (row < 0 || row >= ROW || col < 0 || col >= COL) {
+            System.out.println("Invalid move! Row and column indices must be between 1 and 3.");
+            return false;
+        }
+        if (!board[row][col].equals(" ")) {
+            System.out.println("Invalid move! The cell is already occupied.");
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isWin(String player) {
+        return isRowWin(player) || isColWin(player) || isDiagonalWin(player);
+    }
+    private static boolean isColWin(String player) {
+        for (int col = 0; col < COL; col++) {
+            if (board[0][col].equals(player) && board[1][col].equals(player) && board[2][col].equals(player)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private static boolean isRowWin(String player) {
+        for (int row = 0; row < ROW; row++) {
+            if (board[row][0].equals(player) && board[row][1].equals(player) && board[row][2].equals(player)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private static boolean isDiagonalWin(String player) {
+        return (board[0][0].equals(player) && board[1][1].equals(player) && board[2][2].equals(player)) ||
+                (board[0][2].equals(player) && board[1][1].equals(player) && board[2][0].equals(player));
+    }
+    private static boolean isTie() {
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COL; j++) {
+                if (board[i][j].equals(" ")) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ src/SafeInput.java
@@ -0,0 +1,295 @@
+import java.util.Scanner;
+
+public class SafeInput
+{
+    /**
+     * Get a string which contains at least one character
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return a String response that is not zero length
+     */
+    public static String getNonZeroLenString(Scanner pipe, String prompt)
+    {
+        String retString = "";
+        do
+        {
+            System.out.print("\n" + prompt + ": ");
+            retString = pipe.nextLine();
+        }while(retString.length() == 0); // until we have characters
+
+        return retString;
+    }
+
+    /**
+     * Get an int value within a specified numeric range
+     * @param pipe - Scanner instance to read the data System.in in most cases
+     * @param prompt - input prompt msg should not include range info
+     * @param low - low end of inclusive range
+     * @param high - high end of inclusive range
+     * @return - int value within the inclusive range
+     */
+    public static int getRangedInt(Scanner pipe, String prompt, int low, int high)
+    {
+        int retVal = 0;
+        String trash = "";
+        boolean done = false;
+
+        do {
+            System.out.println("\n" + prompt + "[" + low + "-" + high + "]: ");
+            if (pipe.hasNextInt()) {
+                retVal = pipe.nextInt();
+                pipe.nextLine();
+                if (retVal >= low && retVal <= high) {
+                    done = true;
+                } else {
+                    System.out.println("\nNumber is out of range [" + low + "-" + high + "]: " + retVal);
+                }
+            } else {
+                trash = pipe.nextLine();
+                System.out.println("You must enter an int: " + trash);
+            }
+        } while (!done);
+
+        return retVal;
+
+    }
+
+    /**
+     * Get an int value with no constraints
+     * @param pipe - Scanner instance to read the data System.in in most cases
+     * @param prompt - input prompt msg should not include range info
+     * @return - unconstrained int value
+     */
+    public static int getInt(Scanner pipe, String prompt)
+    {
+        int retVal = 0;
+        String trash = "";
+        boolean done = false;
+
+        do
+        {
+            System.out.println("/n" + prompt + ": ");
+            if(pipe.hasNextInt())
+            {
+                retVal = pipe.nextInt();
+                pipe.nextLine();
+                done = true;
+            }
+            else
+            {
+                trash = pipe.nextLine();
+                System.out.println("You must enter an int: " + trash);
+            }
+        }while(!done);
+
+        return retVal;
+    }
+
+    /**
+     * Get a double value within a specified numeric range
+     * @param pipe - Scanner instance to read the data System.in in most cases
+     * @param prompt - input prompt msg should not include range info
+     * @param low - low end of inclusive range
+     * @param high - high end of inclusive range
+     * @return - int value within the inclusive range
+     */
+    public static double getRangedDouble(Scanner pipe, String prompt, double low, double high)
+    {
+        double retVal = 0;
+        String trash = "";
+        boolean done = false;
+
+        do {
+            System.out.println("\n" + prompt + "[" + low + "-" + high + "]: ");
+            if (pipe.hasNextDouble()) {
+                retVal = pipe.nextDouble();
+                pipe.nextLine();
+                if (retVal >= low && retVal <= high) {
+                    done = true;
+                } else {
+                    System.out.println("\nNumber is out of range [" + low + "-" + high + "]: " + retVal);
+                }
+            } else {
+                trash = pipe.nextLine();
+                System.out.println("You must enter an double " + trash);
+            }
+        } while (!done);
+
+        return retVal;
+
+    }
+
+
+    /**
+     * Get a double value with no constraints
+     * @param pipe - Scanner instance to read the data System.in in most cases
+     * @param prompt - input prompt msg should not include range info
+     * @return - unconstrained int value
+     */
+    public static double getDouble(Scanner pipe, String prompt)
+    {
+        double retVal = 0;
+        String trash = "";
+        boolean done = false;
+
+        do
+        {
+            System.out.println("/n" + prompt + ": ");
+            if(pipe.hasNextDouble())
+            {
+                retVal = pipe.nextDouble();
+                pipe.nextLine();
+                done = true;
+            }
+            else
+            {
+                trash = pipe.nextLine();
+                System.out.println("You must enter an double: " + trash);
+            }
+        }while(!done);
+
+        return retVal;
+    }
+
+
+    /**
+     * Get a [Y/N] confirmation from the user
+     * @param pipe - Scanner instance to read the data System.in in most cases
+     * @param prompt - Input prompt msg for user does not need [Y/N]
+     * @return - true for yes false for no
+     */
+    public static boolean getYNConfirm(Scanner pipe, String prompt)
+    {
+        boolean retVal = true;
+        String response = "";
+        boolean gotAVal = false;
+
+        do {
+            System.out.println("\n" + prompt + " [Y/N] ");
+            response = pipe.nextLine();
+            if (response.equalsIgnoreCase("Y")) {
+                gotAVal = true;
+                retVal = true;
+            } else if (response.equalsIgnoreCase("N")) {
+                gotAVal = true;
+                retVal = false;
+            } else {
+                System.out.println("You must answer [Y/N]! " + response);
+            }
+        } while (!gotAVal);
+
+        return retVal;
+    }
+
+
+    /**
+     * Get a String that matches a RegEx pattern! This is a very powerful method
+     * @param pipe - Scanner instance to read the data System.in in most cases
+     * @param prompt - prompt for user
+     * @param regExPattern - java style RegEx pattern to constrain the input
+     * @return a string that matches the RegEx pattern supplied
+     */
+
+    public static String getRegExString(Scanner pipe, String prompt, String regExPattern)
+    {
+        String response = "";
+        boolean gotAVal = false;
+
+        do
+        {
+            System.out.println("\n" + prompt + ": ");
+            response = pipe.nextLine();
+            if(response.matches(regExPattern))
+            {
+                gotAVal = true;
+            }
+            else
+            {
+                System.out.println("\n" + response + " must match the pattern " + regExPattern);
+                System.out.println("Try again!");
+            }
+        }while(!gotAVal);
+
+        return response;
+    }
+    public static void prettyHeader(String msg)
+    {
+        boolean done = false;
+        int lineLength = 60;
+        int asterisks = 3;
+        int spacesAroundName = ((lineLength - msg.length()) / 2);
+
+        do
+        {
+            if(msg.length() >= 1 && msg.length() <= 60)
+            {
+                done = true;
+            }
+
+            else
+            {
+                System.out.println("\nYour title must be between 1-60 characters!\nTry again.");
+                done = false;
+            }
+
+        }while(!done);
+
+        for(int i = 1; i <= 65; i++)
+        {
+            for(int j = 1; j <= 1; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("");
+        }
+
+        for(int i = 1; i <= 1; i++)
+        {
+            for(int j = 1; j <= 1; j++)
+            {
+                System.out.print("\n***");
+            }
+            System.out.print("");
+        }
+
+        printSpacesAroundName(spacesAroundName);
+        System.out.print(msg);
+        printSpacesAroundName(spacesAroundName);
+
+        for(int i = 62; i <= 62; i++)
+        {
+            for(int j = 62; j <= 62; j++)
+            {
+                System.out.print("***");
+            }
+            System.out.print("\n");
+        }
+
+        for(int i = 1; i <= 65; i++)
+        {
+            for(int j = 3; j <= 3; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("");
+        }
+
+    }
+    private static void printSpacesAroundName(int spacesAroundName)
+    {
+        for (int i = 0; i < spacesAroundName; i++)
+            System.out.print(" ");
+    }
+
+    public static double getAverage(int values[])
+    {
+        int sum = 0;
+        double ave = 0;
+        for(int r = 0; r < values.length; r++)
+        {
+            sum+= values[r];
+        }
+        ave = sum / values.length;
+        return ave;
+    }
+}
